b5e1971340508c217d3f7064b91ccf2f69aaa881
peter <peter@exinto.de>
2012-04-09 10:44:12 -0300

    Bugfix.

--- build_x86.sh
+++ build_x86.sh
@@ -6,5 +6,5 @@ export QMAKESPEC=/opt/onyx/arm/mkspecs/qws/linux-x86-g++/
 mkdir -p build/x86
 cd build/x86
 # XXX detect if we need dbus in the .pro file
-qmake ../../scribble.pro QT+=dbus
+qmake ../../scribble.pro LIBS+=-lonyx_touch QT+=dbus
 make
